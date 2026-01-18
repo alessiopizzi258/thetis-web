778566b6ee104ddcd4b123baa6c74de4e9cf4a9f
Alessio <alessiopizzi258@gmail.com>
2026-01-19 00:21:51 +0100

    Forzo deploy su Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
-module.exports = {
-  content: [],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'thetis-gold': '#D4AF37',
+      }
+    },
   },
   plugins: [],
 }
