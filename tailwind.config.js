13bce0be35f6974486ab1d9218cb7ac2f2d64efa
Alessio <alessiopizzi258@gmail.com>
2026-01-19 00:14:17 +0100

    Aggiunti componenti React e contenuti iniziali

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: [],
   theme: {
-    extend: {
-      colors: {
-        'thetis-gold': '#D4AF37', // Un tocco elegante per un'associazione culturale
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
