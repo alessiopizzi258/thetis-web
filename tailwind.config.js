742b3bb01377f3e2ad981b1ad8c77d6ec76e33a0
Alessio <alessiopizzi258@gmail.com>
2026-01-19 12:09:44 +0100

    seconda modifica

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Assicurati che questi percorsi coprano TUTTI i tuoi file
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
